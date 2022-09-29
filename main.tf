fd7729b2bd2c9aca43f22d8a89546117be438b5b
routhu-dhanunjay-kumar <114486052+routhu-dhanunjay-kumar@users.noreply.github.com>
2022-09-29 18:43:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,8 @@ resource "aws_kms_key" "this" {
   policy                             = var.policy
   tags = var.tags
 }
+
+resource "aws_kms_alias" "this" {
+  name          = "alias/${var.kms_alias_name}"
+  target_key_id = aws_kms_key.this.key_id
+}
